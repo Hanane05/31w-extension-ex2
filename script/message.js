b68d04655cb8c2e278164c734d6e0d42de9a488d
Hanane05 <e2195837@cmaisonneuve.qc.ca>
2022-06-16 11:47:31 -0400

    s5-c10 gestion du bouton modale

--- script/message.js
+++ script/message.js
@@ -1,10 +1,19 @@
 (function(){
+    let elmBoite__modale;
+    let elmBoite__X;
+    function creer_boite_modale() {
+        elmBoite__modale = document.createElement('div')
+        elmBoite__X = document.createElement('button')
+        elmBoite__modale.appendChild(elmBoite__X)
+        elmBoite__X.classList.add('boite__X')
+        elmBoite__X.innerHTML = 'X'
+        elmBoite__modale.classList.add('boite__modale')
+        elmBody = document.querySelector('body')
+        elmBody.appendChild(elmBoite__modale);
+    }
 
-    let elmBoite__modale = document.createElement('div')
+    creer_boite_modale();
     
-    elmBoite__modale.classList.add('boite__modale');
-    elmBody = document.querySelector('body');
-    elmBody.appendChild(elmBoite__modale);
 
 
     // document.querySelector('.boite__modale')
@@ -12,5 +21,12 @@
     
     elmOK.addEventListener('mousedown',  function(){
         console.log('Bravo le script fonctionne');
+        elmBoite__modale.classList.add('boite__modale__ouvrir');
+        console.log(elmBoite__modale.classList);
+    })
+
+
+    elmBoite__X.addEventListener('mousedown', function(){
+        elmBoite__modale.classList.remove('boite__modale__ouvrir')
     })
 })()
